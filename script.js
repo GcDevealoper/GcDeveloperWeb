bd3e8e25addb88301a6c747c1f9c6086762a7385
GcDevealoper <89366058+GcDevealoper@users.noreply.github.com>
2022-03-02 16:13:10 -0400

    Adding New Project and Js ClickMenu

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ function reveal(){
 		}
 	}
 }
+
+//MenuOpenonClick
+
+	
+	var menu = document.querySelector('.menu');
+	var menuBtn = document.querySelector('.MenuBtn');
+	var menuStatus = 1;
+
+	function openMenu(){
+		if (menuStatus == 1){
+
+			menu.classList.add('opened');
+			menuStatus = 2;
+
+		} else if (menuStatus == 2){
+			
+			menu.classList.remove('opened');
+			menuStatus = 1;
+		}
+	}
+
+	menuBtn.addEventListener("click", () => {
+		openMenu();
+	});
+
